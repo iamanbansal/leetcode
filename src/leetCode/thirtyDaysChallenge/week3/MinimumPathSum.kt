4a41694f9996b77c67bdb17ce00c9b18bf988608
Aman Bansal <aman210697@gmail.com>
2020-04-19 10:49:47 +0530

    minimum path sum

---
+++ src/leetCode/thirtyDaysChallenge/week3/MinimumPathSum.kt
@@ -0,0 +1,36 @@
+package leetCode.thirtyDaysChallenge.week3
+
+import kotlin.math.min
+
+/*
+*  LeetCode Problem
+*  https://leetcode.com/problems/minimum-path-sum/
+*/
+
+fun main() {
+    val arr = arrayOf(
+        intArrayOf(1, 3, 1),
+        intArrayOf(1, 5, 1),
+        intArrayOf(4, 2, 1)
+    )
+    println(minPathSum(arr))
+}
+
+fun minPathSum(grid: Array<IntArray>): Int {
+    if (grid.isEmpty())
+        return 0
+
+    for (row in 0 until grid.size) {
+        for (col in 0 until grid[row].size) {
+            if (row == 0 && col == 0) //skip top left cell
+                continue
+            when {
+                row-1 < 0 -> grid[row][col] = grid[row][col] + grid[row][col-1]  // cases for element in top row
+                col-1 <0 -> grid[row][col] = grid[row][col] + grid[row - 1][col] // cases for element in left row
+                else -> grid[row][col] = grid[row][col] + min(grid[row - 1][col], grid[row][col-1])
+            }
+        }
+    }
+    return grid[grid.lastIndex][grid[0].lastIndex]
+}
+
