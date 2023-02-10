a183a45d0ec45e4f829af79277c4b8d19a2023d2
Aman Bansal <aman210697@gmail.com>
2023-02-10 21:03:00 +0530

    construct quad tree and find min ways to change string

--- src/dp/minDistance.kt
+++ src/dp/minDistance.kt
@@ -13,7 +13,7 @@ fun minDistance(word1: String, word2: String): Int {
         else if (index1 >= word1.length) return word2.length - index2
         else if (index2 >= word2.length) return word1.length - index1
 
-        if(dp[index1][index2]!= -1) return dp[index1][index2]
+        if (dp[index1][index2] != -1) return dp[index1][index2]
 
         if (word1[index1] == word2[index2])
             return calculateFurtherMinDistance(index1 + 1, index2 + 1)
@@ -27,4 +27,26 @@ fun minDistance(word1: String, word2: String): Int {
         return dp[index1][index2]
     }
     return calculateFurtherMinDistance(0, 0)
+}
+
+fun minDistance2(word1: String, word2: String): Int {
+    val n = word1.length
+    val m = word2.length
+    val dp = Array(n + 1) { IntArray(m + 1) { -1 } }
+
+    for (i in 0..n) {
+        for (j in 0..m) {
+            if (i == 0) {
+                dp[i][j] = j
+            } else if (j == 0) {
+                dp[i][j] = i
+            } else if (word1[i - 1] == word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1]
+            } else {
+                dp[i][j] = 1 + minOf(dp[i - 1][j - 1], minOf(dp[i - 1][j], dp[i][j - 1]))
+            }
+        }
+    }
+
+    return dp[n][m]
 }
---
+++ src/tree/QuadTree.kt
@@ -0,0 +1,40 @@
+package tree
+
+class Node(var `val`: Boolean, var isLeaf: Boolean) {
+    var topLeft: Node? = null
+    var topRight: Node? = null
+    var bottomLeft: Node? = null
+    var bottomRight: Node? = null
+}
+
+fun construct(grid: Array<IntArray>): Node? {
+    return getQuadTree(grid, 0, 0, grid.size)
+}
+
+fun getQuadTree(grid: Array<IntArray>, i: Int, j: Int, length: Int): Node {
+
+    if (length == 1) {
+        return Node(grid[i][j] == 1, true)
+    }
+
+    val node = Node(false, false)
+    val len = length / 2
+    val topLeft = getQuadTree(grid, i, j, len)
+    val topRight = getQuadTree(grid, i, j + len, len)
+    val bottomLeft = getQuadTree(grid, i + len, j, len)
+    val bottomRight = getQuadTree(grid, i + len, j + len, len)
+
+    if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf &&
+        topLeft.`val` == topRight.`val` && topRight.`val` == bottomLeft.`val` && bottomLeft.`val` == bottomRight.`val`
+    ) {
+        node.isLeaf = true
+        node.`val` = topLeft.`val`
+    } else {
+        node.topLeft = topLeft
+        node.topRight = topRight
+        node.bottomLeft = bottomLeft
+        node.bottomRight = bottomRight
+    }
+
+    return node
+}
